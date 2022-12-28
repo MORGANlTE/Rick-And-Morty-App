f32f12c0d3b948fdeca331b9be1354257b069662
Laurens Arnauts <laurens.arnauts@student.hogent.be>
2022-12-28 15:32:19 +0100

    databinding on main activity

--- app/src/main/java/com/rl/rickandmortyapp/MainActivity.kt
+++ app/src/main/java/com/rl/rickandmortyapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.rl.rickandmortyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rl.rickandmortyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(R.layout.activity_main)
     }
 }
