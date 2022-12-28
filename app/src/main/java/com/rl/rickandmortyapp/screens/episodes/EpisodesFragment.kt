e86d35f69e45a7f9bec83aaad00fda3b4770007f
Laurens Arnauts <laurens.arnauts@student.hogent.be>
2022-12-28 21:24:36 +0100

    navigation via optiemenu

--- app/src/main/java/com/rl/rickandmortyapp/screens/episodes/EpisodesFragment.kt
+++ app/src/main/java/com/rl/rickandmortyapp/screens/episodes/EpisodesFragment.kt
@@ -2,11 +2,11 @@ package com.rl.rickandmortyapp.screens.episodes
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.rl.rickandmortyapp.R
 import com.rl.rickandmortyapp.databinding.FragmentEpisodesBinding
 import com.rl.rickandmortyapp.databinding.FragmentHomepageBinding
@@ -20,7 +20,20 @@ class EpisodesFragment : Fragment() {
 
         val binding = DataBindingUtil.inflate<FragmentEpisodesBinding>(inflater,
             R.layout.fragment_episodes,container,false)
-
+        //set the menu to visible
+        setHasOptionsMenu(true)
         return binding.root
+    }
+    //inflate the menu resource file
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    //override to set the action on the menubuttons to their respective destination
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.
+        onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/rl/rickandmortyapp/screens/main/HomepageFragment.kt
+++ app/src/main/java/com/rl/rickandmortyapp/screens/main/HomepageFragment.kt
@@ -3,12 +3,11 @@ package com.rl.rickandmortyapp.screens.main
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.rl.rickandmortyapp.R
 import com.rl.rickandmortyapp.databinding.FragmentHomepageBinding
 
@@ -29,7 +28,22 @@ class HomepageFragment : Fragment() {
         //set the onclick listeners for the buttons
         setupButtons()
 
+        //set the menu to visible
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    //inflate the menu resource file
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    //override to set the action on the menubuttons to their respective destination
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.
+        onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
     private fun setupButtons() {
