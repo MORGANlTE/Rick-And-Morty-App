9742b882b494f85c90fb0569ac69d4f25dbdd364
Remco <79316889+remcodesmedt@users.noreply.github.com>
2022-12-29 15:22:33 +0100

    insert fake characters in viewmodel, db works

--- app/src/main/java/com/rl/rickandmortyapp/database/CharacterDatabaseDao.kt
+++ app/src/main/java/com/rl/rickandmortyapp/database/CharacterDatabaseDao.kt
@@ -3,18 +3,15 @@ package com.rl.rickandmortyapp.database
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 
 @Dao
 interface CharacterDatabaseDao {
     //to insert characters from api
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(character: Character)
-
-    //to update this db with up-to-date values from api
-    @Update
-    suspend fun update(character: Character)
 
     //get all of the characters in the db
     @Query("SELECT * FROM character_table ORDER BY characterId DESC")
--- app/src/main/java/com/rl/rickandmortyapp/screens/characters/CharacterViewModel.kt
+++ app/src/main/java/com/rl/rickandmortyapp/screens/characters/CharacterViewModel.kt
@@ -5,28 +5,37 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.rl.rickandmortyapp.database.Character
 import com.rl.rickandmortyapp.database.CharacterDatabaseDao
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
 
 class CharacterViewModel(dataSource: CharacterDatabaseDao, application: Application) : ViewModel() {
 
-//    val characters = dataSource.getAllCharacters()
-
-    private val _characters = MutableLiveData<List<Character>>()
-    val characters: LiveData<List<Character>>
-        get() = _characters
+    val database = dataSource
+    val characters = database.getAllCharacters()
 
     init {
-        _characters.value = listOf(
-            Character(
-                0L, "de rick", "alive",
-                "human", "imgurl", "earth"
-            ),
-            Character(
-                1L, "de rick2", "alive2",
-                "human2", "imgurl2", "earth2"
+        //insert 2 characters into the db
+        viewModelScope.launch {
+            insert(
+                Character(
+                    1L, "de rick", "alive",
+                    "human", "imgurl", "earth"
+                )
             )
-        )
+            insert(
+                Character(
+                    2L, "de rick2", "alive2",
+                    "human2", "imgurl2", "earth2"
+                )
+            )
+        }
+    }
+
+    private suspend fun insert(character: Character) {
+        database.insert(character)
     }
 
 
