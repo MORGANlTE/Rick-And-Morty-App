2c14c35e0bbe65b41771b015f21be82518df6968
Remco <79316889+remcodesmedt@users.noreply.github.com>
2022-12-29 12:47:55 +0100

    added navmenu to character fragment

--- app/src/main/java/com/rl/rickandmortyapp/screens/characters/CharacterFragment.kt
+++ app/src/main/java/com/rl/rickandmortyapp/screens/characters/CharacterFragment.kt
@@ -1,11 +1,13 @@
 package com.rl.rickandmortyapp.screens.characters
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.rl.rickandmortyapp.R
+import com.rl.rickandmortyapp.databinding.FragmentCharacterBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +25,24 @@ class CharacterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_character, container, false)
+        val binding = DataBindingUtil.inflate<FragmentCharacterBinding>(inflater,
+            R.layout.fragment_character,container,false)
+        //set the menu to visible
+        setHasOptionsMenu(true)
+        return binding.root
     }
+
+    //inflate the menu resource file
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    //override to set the action on the menubuttons to their respective destination
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.
+        onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
 }
