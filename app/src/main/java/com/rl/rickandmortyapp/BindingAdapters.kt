7fdc2e613243f6af790345f5912902dde497add7
Laurens Arnauts <laurens.arnauts@student.hogent.be>
2023-01-02 13:26:37 +0100

    filez

--- app/src/main/java/com/rl/rickandmortyapp/BindingAdapters.kt
+++ app/src/main/java/com/rl/rickandmortyapp/BindingAdapters.kt
@@ -4,7 +4,11 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import coil.load
-
+/**
+ * The binding adapter for the imageUrls
+ * @param imgView The imageview to update
+ * @param imgUrl The url to set the image
+ */
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
